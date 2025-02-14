cee99fabc2d61e8876cfd19fc3a0970c868c14e8
kimdohyun <dohyun2008@kakao.com>
2025-02-14 15:49:30 -0300

    make studnet list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import { useState } from 'react'
 import qwerty from './assets/qwerty.svg'
 import curriculum from './assets/curriculum.svg'
+
+import student1 from './assets/student/김도현.png'
+import student2 from './assets/student/염승엽.png'
+import student3 from './assets/student/쿼티짱.png'
+import student4 from './assets/student/최진호.png'
+import student5 from './assets/student/남윤지.png'
+import student6 from './assets/student/쿼티짱.png'
+import student7 from './assets/student/쿼티짱.png'
+import student8 from './assets/student/쿼티짱.png'
+import student9 from './assets/student/쿼티짱.png'
+
 import './App.css'
 
 function App() {
@@ -36,7 +47,7 @@ function App() {
 
         <section className="curriculum">
 
-          <div className="container">
+          <div className="curriculum-container">
             <h1>풀스택 개발을 위한<br /><div className='qwerty-color'>QWERTY</div>만의 커리큘럼</h1>
             <p>
                 QWERTY 에서는 웹 개발에 필요한 <br/>
@@ -48,7 +59,7 @@ function App() {
               </p>
           </div>
 
-          <div className='curriculum-container'>
+          <div className='curriculumlist-container'>
             <img src={curriculum} alt="Qwerty" className="curriculum-logo" />
             <ul className='curriculum-list'>
               <li>HTML, CSS, Java Script</li>
@@ -58,6 +69,83 @@ function App() {
             <a href='#' className='curriculum-btn'>커리큘럼 확인하기</a>
           </div>
         </section>
+
+        <section className="members">
+          <h1>부원 소개</h1>
+          <h2>QWERTY 6기</h2>
+          <div className="members-grid">
+            <div className="member">
+              <img src={student1} alt="Student 1" />
+              <div className="member-info">
+                <h2>김도현</h2>
+                <p>backend developer</p>
+              </div>
+            </div>
+            <div className="member">
+              <img src={student2} alt="Student 2" />
+              <div className="member-info">
+                <h2>염승엽</h2>
+                <p>부장</p>
+              </div>
+            </div>
+            <div className="member">
+              <img src={student3} alt="Student 3" />
+              <div className="member-info">
+                <h2>정여진</h2>
+                <p>부부장</p>
+              </div>
+            </div>
+            <div className="member">
+              <img src={student4} alt="Student 4" />
+              <div className="member-info">
+                <h2>최진호</h2>
+                <p>buwon</p>
+              </div>
+            </div>
+            <div className="member">
+              <img src={student5} alt="Student 5" />
+              <div className="member-info">
+                <h2>남윤지</h2>
+                <p>게임만드는<br/>피곤한 그림쟁이</p>
+              </div>
+            </div>
+            <div className="member">
+              <img src={student6} alt="Student 6" />
+              <div className="member-info">
+                <h2>김동완</h2>
+                <p>김동완</p>
+              </div>
+            </div>
+            <div className="member">
+              <img src={student7} alt="Student 7" />
+              <div className="member-info">
+                <h2>이동규</h2>
+                <p>이동규</p>
+              </div>
+            </div>
+            <div className="member">
+              <img src={student8} alt="Student 8" />
+              <div className="member-info">
+                <h2>김나우</h2>
+                <p>김나우</p>
+              </div>
+            </div>
+            <div className="member">
+              <img src={student9} alt="Student 9" />
+              <div className="member-info">
+                <h2>최진호</h2>
+                <p>최진호</p>
+              </div>
+            </div>
+          </div>
+
+        </section>
+
+        <section className="apply">
+          <h1>문의 및 지원 안내</h1>
+          
+        </section>
+
 
     </>
   )
