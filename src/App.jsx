1e9c083a67938b74d7d3d1234208ceec9bd8ceb2
kimdohyun <dohyun2008@kakao.com>
2025-02-14 13:04:26 -0300

    fuck-css

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import qwerty from './assets/qwerty.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <header className="App-header">
+            <img src={qwerty} alt="Qwerty" className="header-logo" />
+            <ul className={"nav-links"}>
+                <li><a href="#">Home</a></li>
+                <li><a href="#ca">커리큘럼</a></li>
+                <li><a href="#">부원</a></li>
+                <li><a href="#">지원하기</a></li>
+            </ul>
+        </header>
+
+        <section className="home">
+          <div className="container">
+                <h1>정제되고 체계적으로. <br /> <div className='qwerty-color'>QWERTY</div> 답게.</h1>
+                <p>
+                  QWERTY 는 선린인터넷 고등학교의 일반 웹 동아리 입니다.<br />
+                  2023년에 신설 동아리로 보다 정제되고 체계적인<br />
+                  활동을 목표로 합니다.
+                </p>
+            </div>
+
+            <div className='logo-container'>
+              <img src={qwerty} alt="Qwerty" className="pages-logo" />
+              <h1 className='qwerty-color'>QWERTY</h1>
+            </div>
+        </section>
+
     </>
   )
 }
