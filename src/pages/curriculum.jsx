3239d7575e9ad93a9dfd5cf18eb5b2e431c28ba5
kimdohyun <dohyun2008@kakao.com>
2025-02-17 12:16:24 -0300

    .netlifyignore 및 netlify.toml 파일 삭제, curriculum.jsx에서 DM 요청 메시지 수정

--- src/pages/curriculum.jsx
+++ src/pages/curriculum.jsx
@@ -3,7 +3,7 @@ import './main.css'
 function curriculum() {
   return (
     <div>
-      <h1>DM으로 문의 부탁드려요. <br />🙏🙏🙏</h1>
+      <h1>DM으로 문의 부탁드려요.<br />🙏🙏🙏</h1>
       <a href="https://www.instagram.com/yeoum.sy/">DM보내기</a>
     </div>
   )
